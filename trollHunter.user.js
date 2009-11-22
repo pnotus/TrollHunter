489fce45ab38b680ac33237e2fe80c44396035f2
Alice <Alice@Fantomen>
2009-11-22 18:35:55 +0100

    Tog bort include till localhost

--- trollHunter.user.js
+++ trollHunter.user.js
@@ -3,7 +3,6 @@
 // @description	        hunt down tolls on idg.se
 // @include				http://idg.se/*
 // @include				http://*.idg.se/*
-// @include				http://localhost/TrollHunter/*
 // @require				http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // @require				http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js
 // ==/UserScript==
